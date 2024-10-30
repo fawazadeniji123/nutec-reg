3f7155647181356064d7c3aaf79e58e139114602
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-10-30 02:47:08 +0100

    feat: add skeleton loading ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import UserProfile from "@/components/shared/user-profile";
-import { useUserContext } from "@/context/AuthContext";
 
-export default function Home() {
-  const { user } = useUserContext();
-  return <UserProfile {...user} />;
+export default async function Home() {
+  
+  return <UserProfile />;
 }
